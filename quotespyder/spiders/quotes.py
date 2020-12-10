57c0cb05eb02b12d5b640d4d5048295093b358c6
ResXBocchi <bocchi.dev@gmail.com>
2020-12-10 13:35:09 -0300

    changing from CSS to XPATH

--- quotespyder/spiders/quotes.py
+++ quotespyder/spiders/quotes.py
@@ -8,10 +8,23 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        title = response.css('title::text').extract_first()
-        tags = response.css('.tag-item a::text').extract()
+        # title = response.css('title::text').extract_first()
+        # tags = response.css('.tag-item a::text').extract()
+
+        # yield{
+        #     'Title':title,
+        #     'Tags':tags
+        # }
+    
+        quotes = response.css('.quote')
+
+        for quote in quotes:
+            text = quotes.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quotes.xpath('.//*[@class="author"]/text()').extract_first()
+            tags = quotes.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
 
         yield{
-            'Title':title,
+            'Text':text,
+            'Author':author,
             'Tags':tags
         }
