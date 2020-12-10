f5d2607ddd8ccc57aca9997b28cd0ded2d2007e7
ResXBocchi <bocchi.dev@gmail.com>
2020-12-10 12:11:17 -0300

    first spyder

---
+++ quotespyder/spiders/quotes.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        title = response.css('title::text').extract_first()
+        tags = response.css('.tag-item a::text').extract()
+        
+    yield{
+        'Title':title;
+        'Tags':tags;
+    }
